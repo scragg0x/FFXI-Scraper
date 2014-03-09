92ab49c804c0dbfd75418d960394cdecb58dec34
Matthew <scragg@gmail.com>
2014-03-09 17:59:51 -0500

    handle value errors with main/sub job level

--- ffxiscraper/__init__.py
+++ ffxiscraper/__init__.py
@@ -208,13 +208,18 @@ class FFXiScraper(Scraper):
                 data['equip_data'][constants.FFXI_SLOTS[i]]['level'] = data['item_levels'][i]
 
         if data['main_job']:
-            data['main_level'] = int(data['main_job'].split(" ")[-1])
-            data['main_job'] = constants.FFXI_JOBS_REVERSE[' '.join(data['main_job'].split(" ")[0:-1])].upper()
+            try:
+                data['main_level'] = int(data['main_job'].split(" ")[-1])
+                data['main_job'] = constants.FFXI_JOBS_REVERSE[' '.join(data['main_job'].split(" ")[0:-1])].upper()
+            except ValueError:
+                del data['main_job']
 
         if data['sub_job']:
-            data['sub_level'] = int(data['sub_job'].split(" ")[-1])
-            data['sub_job'] = constants.FFXI_JOBS_REVERSE[' '.join(data['sub_job'].split(" ")[0:-1])].upper()
-
+            try:
+                data['sub_level'] = int(data['sub_job'].split(" ")[-1])
+                data['sub_job'] = constants.FFXI_JOBS_REVERSE[' '.join(data['sub_job'].split(" ")[0:-1])].upper()
+            except ValueError:
+                del data['sub_job']
 
         data['avatar_url'] = 'http://fanzone.playonline.com' + data['avatar_url']
 
